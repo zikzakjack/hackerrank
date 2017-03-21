579b84b9cd44b223f5bbd059a44fa82560215da4
balasuks <balasuks@W01BCJ28.UBSPROD.MSAD.UBS.NET>
2017-03-21 10:42:08 -0400

    30 Days of Code - Day 01

---
+++ src/org/zikzakjack/hackerrank/days/Day01.java
@@ -0,0 +1,16 @@
+package org.zikzakjack.hackerrank.days;
+
+import java.util.Scanner;
+
+public class Day01 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+//		String inputString = scanner.useDelimiter("\\r\n").next();
+		String inputString = scanner.nextLine();
+		scanner.close();
+		System.out.println("Hello, World.");
+		System.out.println(inputString);
+	}
+
+}
