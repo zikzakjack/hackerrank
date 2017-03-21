3d2fd92456cf5030dc54feb9b3ae43c4dc24179d
balasuks <balasuks@W01BCJ28.UBSPROD.MSAD.UBS.NET>
2017-03-21 15:19:52 -0400

    Practice Challenge : Java Int to String

---
+++ src/org/zikzakjack/hackerrank/JavaIntToString.java
@@ -0,0 +1,24 @@
+package org.zikzakjack.hackerrank;
+
+import java.util.Scanner;
+
+public class JavaIntToString {
+
+	public static void main(String[] args) {
+
+		try {
+			Scanner in = new Scanner(System.in);
+			int n = in.nextInt();
+			in.close();
+			String s = Integer.toString(n);
+			if (n == Integer.parseInt(s)) {
+				System.out.println("Good job");
+			} else {
+				System.out.println("Wrong answer");
+			}
+		} catch (Exception ex) {
+			System.out.println("Unsuccessful Termination");
+		}
+	}
+
+}
